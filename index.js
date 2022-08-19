fe7dc0ae9a70f7bea0469a47d6c239be49b74b5b
Shanda Anny <85841475+shanda0409@users.noreply.github.com>
2022-08-19 16:30:30 +0000

    updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log('Welcome to the GUESS NUMBER GAME 🤞🤞\n\nPlease enter a number between 1 to 100 👊\n\n');
-let number = Number.parseInt(Math.random() + 4*15);
+let number = Number.parseInt(Math.random() + 4 * 15);
 let chances = 0;
 let userNumber;
 do {
